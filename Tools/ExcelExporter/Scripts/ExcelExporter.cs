aef156f50fd0e7231ca036ee64b01c8369759fa0
kaclok <kaclok@users.noreply.github.com>
2021-06-21 19:17:32 +0800

    ExcelExporter.cs

---
+++ Tools/ExcelExporter/Scripts/ExcelExporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelExporter {
+    public partial class ExcelExporter : Form {
+        public ExcelExporter() {
+            InitializeComponent();
+        }
+
+    }
+}
