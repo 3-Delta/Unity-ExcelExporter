381db5115479dfe21932cc63f5dfc37203336835
kaclok <kaclok@users.noreply.github.com>
2021-06-22 02:39:38 +0800

    测试svnhelper的使用

--- Tools/SVN/Scripts/Program.cs
+++ Tools/SVN/Scripts/Program.cs
@@ -6,7 +6,11 @@ namespace SVN
     {
         static void Main(string[] args)
         {
-            SVNHelper.Commit("");
+            // 全路径不配置 worlDir也可以
+            SVNHelper.Commit("I:\\Project\\SVNRepo\\test.txt");
+
+            // "I:\\Project\\SVNRepo"的worlDir +  "\\test.txt" 一起工作
+            // SVNHelper.Commit("test.txt");
 
             Console.WriteLine("Hello World!");
 
--- Tools/SVN/Scripts/SVN.cs
+++ Tools/SVN/Scripts/SVN.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace SVN {
     // 处理SVN
-    public static class SVNHelper {
+    public class SVNHelper {
         public static string workDirectory { get; set; } = null;
 
         public static void Revert(IList<string> paths) {
@@ -38,22 +38,26 @@ namespace SVN {
 
         private static void _SVNCmd(string arg) {
             // 需要安装 svn命令行
-            CMDProcess.Exec(workDirectory, "TortoiseProc.exe", arg, true);
+            CMDProcess.Exec(workDirectory, "TortoiseProc.exe", arg);
         }
     }
 
     // 处理进程
-    public static class CMDProcess {
-        public static void Exec(string workDirectory, string fileName, string arg, bool waitForExit = true) {
+    public class CMDProcess {
+        public static void Exec(string workDirectory, string fileName, string arg, bool waitForExit = false) {
             Process process = new Process();
             // 需要安装 svn命令行
             process.StartInfo.FileName = fileName;
             process.StartInfo.Arguments = arg;
             process.StartInfo.UseShellExecute = true;
+
+            // workingDirectory的意思是：如果args是全路径，则不设置workDir也行，如果只是一个文件名，则必须设置workDir
+            // 可以理解为：workDirectory + fileName = 最终的路径
             process.StartInfo.WorkingDirectory = workDirectory;
 
             process.Start();
             if (waitForExit) {
+                // waitForExit的意思是：A启动一个进程B之后，阻塞当前进程A直到B退出，A才能重新激活
                 process.WaitForExit();
             }
         }
