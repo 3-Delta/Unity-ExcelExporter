a3aa65a6b35016b6bb4c5243d50fdd86e5030dcc
kaclok <kaclok@users.noreply.github.com>
2021-06-24 20:50:43 +0800

    ExcelParser excel解析器

---
+++ Tools/ExcelParser/Scripts/ExcelReader/ExcelReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelParser {
+    public class ExcelHelper {
+        public static readonly string[] builtinTypeNames = new string[] { "int", "uint" };
+    }
+
+    public class Sheet { 
+    }
+
+    // 表格字段
+    public class Field {
+        public string name;
+        public string type;
+
+        public bool IsBuiltin { get; }
+        public bool IsArray { get; }
+    }
+
+    public class ExcelReader {
+
+    }
+}
---
+++ Tools/ExcelParser/Scripts/ExcelReader/JsonWriter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelParser {
+    public class JsonWriter {
+
+    }
+}
